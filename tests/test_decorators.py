d6c4ed5e9cd93eeb162523538ea02e864e8b25c9
slaily <slavov.iliyan96@gmail.com>
2019-08-23 15:17:03 +0300

    Refactor test that checks the file extension to pass

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,6 +1,10 @@
+from pyreqtest import decorators
+
 
 def test_check_file_extension():
-    func = check_file_extension(lambda file_extension: file_extension)
+    func = decorators.check_file_extension(
+        lambda file_extension: file_extension
+    )
     func_result = func('.json')
 
     assert func_result == '.json'
