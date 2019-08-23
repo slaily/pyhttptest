68f22f9e05f4d38faf3bb9804c153ca151465b70
slaily <slavov.iliyan96@gmail.com>
2019-08-23 17:06:00 +0300

    Refactor file extension error exception test case to pass

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,3 +1,6 @@
+from pyreqtest import exceptions
+
+
 def test_file_extensior_error_exception():
     file_extension_error = exceptions.FileExtensionError('.yaml')
     exception_message = "A file extension '.yaml' is not supported"
