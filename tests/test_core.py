48b948e7c65e97d37fe07a17bff35d5d32c5e079
slaily <slavov.iliyan96@gmail.com>
2019-08-30 15:58:52 +0300

    Add a test case for loading unsupported file format, which raises an exception

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,17 @@
 import pytest
 
 from pyreqtest import core
+from pyreqtest.exceptions import FileExtensionError
 
 
 def test_load_json_from_file():
     json_dict = core.load_json_from_file('data/test_data.json')
 
     assert isinstance(json_dict, dict)
+
+
+def test_load_json_from_file_format_type_yaml():
+    with pytest.raises(FileExtensionError) as exc:
+        core.load_json_from_file('data/test_data.yaml')
+
+    assert "is not supported" in str(exc.value)
