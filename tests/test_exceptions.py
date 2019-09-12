633acfd0b1f27bc085eab416029e2c54be8c84c4
slaily <slavov.iliyan96@gmail.com>
2019-09-12 16:58:58 +0300

    Fix failing test case regarding file extension error

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -3,6 +3,9 @@ from pyreqtest import exceptions
 
 def test_file_extensior_error_exception():
     file_extension_error = exceptions.FileExtensionError('.yaml')
-    exception_message = "A file extension '.yaml' is not supported"
+    exception_message = (
+        "A file extension '.yaml' is not supported. "
+        "Only a file with '.json' extension is supported"
+    )
 
     assert file_extension_error.message == exception_message
