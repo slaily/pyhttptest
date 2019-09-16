3acdea9a098c368e76b8bcffdc2dee425de73e44
slaily <slavov.iliyan96@gmail.com>
2019-09-16 14:14:22 +0300

    Add test case for validating invalid JSON data under schema

--- tests/http_schemas/test_base_schema.py
+++ tests/http_schemas/test_base_schema.py
@@ -1,4 +1,7 @@
+import pytest
+
 from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 
 from pyreqtest.http_schemas.base_schema import base_schema
 
@@ -14,3 +17,18 @@ def test_schema_with_valid_data():
     result = validate(instance=data, schema=base_schema)
 
     assert result is None
+
+
+def test_schema_with_invalid_data():
+    with pytest.raises(ValidationError) as exc:
+        # Not including a required property 'endpoint'
+        # for the schema into the `dict` below
+        data = {
+            'name': 'Test',
+            'verb': 'Base',
+            'host': 'http://test.com',
+        }
+
+        validate(instance=data, schema=base_schema)
+
+    assert 'required property' in str(exc.value)
