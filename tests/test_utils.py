b63fe3c69028cf7373b1365b559df471c00dcf31
slaily <slavov.iliyan96@gmail.com>
2019-09-27 12:56:16 +0300

    Add a test case for extracting properties values of type dict from JSON data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,12 +5,12 @@ from pyreqtest import utils
 
 def test_extract_properties_values_from_json():
     json_data = {
-        "name": "TEST: List all users",
-        "verb": "GET",
-        "endpoint": "users",
-        "host": "HTTP://localhost:8080",
-        "headers": {
-            "Accept-Language": "en-US"
+        'name': 'TEST: List all users',
+        'verb': 'GET',
+        'endpoint': 'users',
+        'host': 'HTTP://localhost:8080',
+        'headers': {
+            'Accept-Language': 'en-US'
         }
     }
     json_keys = ('verb', 'endpoint', 'host')
@@ -48,3 +48,19 @@ def test_extract_properties_values_from_json_with_wrong_keys_format():
     part_of_exc_msg = 'Not a type of {type}'.format(type=type(json_keys))
 
     assert part_of_exc_msg in str(exc.value)
+
+
+def test_extract_properties_values_of_type_dict_from_json():
+    json_data = {
+        'host': 'HTTP://localhost:8080',
+        'headers': {
+            'Accept-Language': 'en-US'
+        }
+    }
+    json_keys = ('headers',)
+    extracted_keys_values = utils.extract_properties_values_of_type_dict_from_json(
+        json_data,
+        json_keys
+    )
+
+    assert 'headers' in extracted_keys_values
