9b3162fd2aed2e759bd3ac78cd54f9d952606389
slaily <slavov.iliyan96@gmail.com>
2019-09-27 09:15:46 +0300

    Add a test case to cover the exceptions raised from extract JSON Keys values functionality

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pyreqtest import utils
 
 
@@ -12,11 +14,37 @@ def test_extract_json_keys_values():
         }
     }
     json_keys = ('verb', 'endpoint', 'host')
-    json_data_keys_values = (
+    extracted_keys_values = utils.extract_json_keys_values(
+        json_data,
+        json_keys
+    )
+    expected_keys_values = (
         json_data['verb'],
         json_data['endpoint'],
         json_data['host']
     )
-    args = utils.extract_json_keys_values(json_data, json_keys)
 
-    assert sorted(json_data_keys_values) == sorted(args)
+    assert sorted(expected_keys_values) == sorted(extracted_keys_values)
+
+
+def test_extract_json_keys_values_with_wrong_data_format():
+    json_data = 'key: value'
+
+    with pytest.raises(TypeError) as exc:
+        utils.extract_json_keys_values(json_data, ())
+
+    part_of_exc_msg = 'Not a type of {type}'.format(type=type(json_data))
+
+    assert part_of_exc_msg in str(exc.value)
+
+
+def test_extract_json_keys_values_with_wrong_keys_format():
+    json_data = {'name': 'test', 'verb': 'GET'}
+    json_keys = 'name, verb'
+
+    with pytest.raises(TypeError) as exc:
+        utils.extract_json_keys_values(json_data, json_keys)
+
+    part_of_exc_msg = 'Not a type of {type}'.format(type=type(json_keys))
+
+    assert part_of_exc_msg in str(exc.value)
