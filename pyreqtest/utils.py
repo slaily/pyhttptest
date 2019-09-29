56596296af8f9f750d08c0da91f2662e7817b9ed
slaily <slavov.iliyan96@gmail.com>
2019-09-29 21:00:28 +0300

    Assign decorator to functions extracting JSON properties and refactor docstrings

--- pyreqtest/utils.py
+++ pyreqtest/utils.py
@@ -1,3 +1,7 @@
+from pyreqtest.decorators import validate_extract_json_properties_func_args
+
+
+@validate_extract_json_properties_func_args
 def extract_properties_values_from_json(data, keys):
     """Extracts properties values from the JSON data.
 
@@ -5,9 +9,11 @@ def extract_properties_values_from_json(data, keys):
 
         Each of key/value pairs into JSON conventionally referred
         to as a "property". More information about this convention follow
-        `JSON Schema documentation <https://json-schema.org/understanding-json-schema/reference/object.html>`_.  # noqa
+        `JSON Schema documentation <https://json-schema.org/understanding-json-schema/reference/object.html>`_.
 
     Passing ``data`` argument for an example:
+
+    .. code-block:: python
 
         data = {
             "verb": "GET",
@@ -18,47 +24,30 @@ def extract_properties_values_from_json(data, keys):
 
     along with ``keys`` argument for an example:
 
-        keys = ('verb', 'endpoint', host')
+    .. code-block:: python
+
+        keys = ('verb', 'endpoint', 'host')
 
     Iterating over ``keys`` parameter values and
     extracts the property value of ``data`` parameter by key with the
     exact same value.
 
     Result:
+
+    .. code-block:: python
+
         ('GET', 'users, 'http://localhost:8080')
 
     :param dict data: Data within а JSON format.
-    :param tuple|list|set keys: Iterable with values of type str.
+    :param tuple|list|set keys: Iterable with values of type `str`.
 
-    :returns: Packaged values within а tuple.
-    :rtype: tuple
-
-    :raises TypeError: If the data is not a dict.
-    :raises TypeError: If the keys is not a type of (tuple, list, set).
+    :returns: Packaged values within а `tuple`.
+    :rtype: `tuple`
     """
-    if not isinstance(data, dict):
-        raise TypeError(
-            (
-                "Passed 'data' param argument, must be of "
-                "data type 'dict'. Not a type of {type}.".format(
-                    type=type(data)
-                )
-            )
-        )
-
-    if not isinstance(keys, (tuple, list, set)):
-        raise TypeError(
-            (
-                "Passed 'keys' param argument, must be one of: "
-                "(tuple, list, set) data types. Not a type of {type}.".format(
-                    type=type(keys)
-                )
-            )
-        )
-
     return tuple(data[key] for key in keys if key in data)
 
 
+@validate_extract_json_properties_func_args
 def extract_properties_values_of_type_dict_from_json(data, keys):
     """Extracts properties values of type `dict` from the JSON data.
 
@@ -103,10 +92,10 @@ def extract_properties_values_of_type_dict_from_json(data, keys):
         }
 
     :param dict data: Data within а JSON format.
-    :param tuple|list|set keys: Iterable with values of type str.
+    :param tuple|list|set keys: Iterable with values of type `str`.
 
-    :returns: Packaged key/value pairs within а dict.
-    :rtype: dict
+    :returns: Packaged key/value pairs within а `dict`.
+    :rtype: `dict`
     """
     return {
         key: data[key] for key in keys
