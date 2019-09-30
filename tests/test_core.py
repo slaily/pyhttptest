aff14e1c466175ced03c634dc54ecc2eebc977bb
slaily <slavov.iliyan96@gmail.com>
2019-09-30 11:07:42 +0300

    Add files with data used for tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,20 +5,20 @@ from pyreqtest.exceptions import FileExtensionError
 
 
 def test_load_json_from_file():
-    json_dict = core.load_json_from_file('data/test_data.json')
+    json_dict = core.load_json_from_file('data/HTTP_GET.json')
 
     assert isinstance(json_dict, dict)
 
 
 def test_load_json_from_file_with_not_supported_file_extension():
     with pytest.raises(FileExtensionError) as exc:
-        core.load_json_from_file('data/test_data.yaml')
+        core.load_json_from_file('data/HTTP_GET.yaml')
 
     assert "is not supported" in str(exc.value)
 
 
 def test_extract_json_data():
-    data = core.load_json_from_file('data/test_data.json')
+    data = core.load_json_from_file('data/HTTP_GET.json')
     required_args, optional_kwargs = core.extract_json_data(data)
 
     assert isinstance(required_args, tuple) and isinstance(optional_kwargs, dict)
