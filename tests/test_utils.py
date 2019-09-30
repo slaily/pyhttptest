2d4db2ea599084fb9fe3f7c477c4ea1dfb8ca88d
slaily <slavov.iliyan96@gmail.com>
2019-09-30 14:46:04 +0300

    Add test cases to cover all branches in the URL preparing function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,7 @@
+import pytest
+
+from http.client import InvalidURL
+
 from pyreqtest import utils
 
 
@@ -47,3 +51,51 @@ def test_prepare_url():
     url = utils.prepare_url(host, endpoint)
 
     assert url == 'http://localhost:8080/users'
+
+
+def test_prepare_url_with_none_type_arg():
+    url = utils.prepare_url('http://localhost:8080', None)
+
+    assert url is None
+
+
+def test_prepare_url_with_host_arg_ends_with_backslash():
+    host = 'http://localhost:8080/'
+    endpoint = 'users'
+    url = utils.prepare_url(host, endpoint)
+
+    assert url == 'http://localhost:8080/users'
+
+
+def test_prepare_url_with_endpoint_arg_starts_with_backslash():
+    host = 'http://localhost:8080'
+    endpoint = '/users'
+    url = utils.prepare_url(host, endpoint)
+
+    assert url == 'http://localhost:8080/users'
+
+
+def test_prepare_url_with_both_host_and_endpoint_args_contains_backslash():
+    host = 'http://localhost:8080/'
+    endpoint = '/users'
+    url = utils.prepare_url(host, endpoint)
+
+    assert url == 'http://localhost:8080/users'
+
+
+def test_prepare_url_with_invalid_host_arg_format():
+    with pytest.raises(InvalidURL) as exc:
+        host = 'localhost.com'
+        endpoint = 'users'
+        utils.prepare_url(host, endpoint)
+
+    assert 'Invalid URL' in str(exc.value)
+
+
+def test_prepare_url_with_not_supported_url_scheme():
+    with pytest.raises(InvalidURL) as exc:
+        host = 'ftp://localhost.com'
+        endpoint = 'users'
+        utils.prepare_url(host, endpoint)
+
+    assert 'Invalid URL scheme' in str(exc.value)
