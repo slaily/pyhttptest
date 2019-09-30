9c3d234113009cc474ed1a992ca69dd6e85b0626
slaily <slavov.iliyan96@gmail.com>
2019-09-30 10:46:28 +0300

    Define constants for JSON Schema required and optional keys

--- pyreqtest/constants.py
+++ pyreqtest/constants.py
@@ -1,1 +1,13 @@
 JSON_FILE_EXTENSION = '.json'
+
+REQUIRED_SCHEMA_KEYS = (
+    'name',
+    'verb',
+    'endpoint',
+    'host'
+)
+
+OPTIONAL_SCHEMA_KEYS = (
+    'headers',
+    'query_string'
+)
