1fb548cdcb1d2ac9f2e41677b24a7c8865b816f9
slaily <slavov.iliyan96@gmail.com>
2019-10-24 16:36:40 +0300

    Define JSON Schema use to validate HTTP POST Request data

---
+++ pyhttptest/http_schemas/post_schema.py
@@ -0,0 +1,16 @@
+from copy import deepcopy
+
+from pyhttptest.http_schemas.base_schema import base_schema
+
+
+# A ``dict`` schema that copies :file:`pyhttptest/http_schemas/base_schema.py`
+# schema and extends it with properties related for a structure specification
+# sending an HTTP POST Request.
+post_schema = deepcopy(base_schema)
+post_schema['properties'].update(
+    {
+        'payload': {
+            'type': 'object'
+        }
+    }
+)
