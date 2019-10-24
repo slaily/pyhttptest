57008fe3b7370bfd5b30e2e5c3dabf9d6ea3e6ad
slaily <slavov.iliyan96@gmail.com>
2019-10-24 16:25:56 +0300

    Add a test case for data validation against HTTP POST JSON Schema

---
+++ tests/http_schemas/test_post_schema.py
@@ -0,0 +1,21 @@
+from jsonschema import validate
+
+from pyhttptest.http_schemas.post_schema import post_schema
+
+
+def test_schema_with_valid_data():
+    data = {
+        'name': 'Test',
+        'verb': 'POST',
+        'endpoint': 'users',
+        'host': 'http://test.com',
+        'payload': {
+            'user': {
+                'id': 1
+            }
+        }
+    }
+
+    result = validate(instance=data, schema=post_schema)
+
+    assert result is None
