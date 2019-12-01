539026262fa50b0f2ff8fd0e16fe6a4bb3934ad1
slaily <slavov.iliyan96@gmail.com>
2019-12-01 09:48:35 +0200

    Add test case for a function that wraps the logic for executing multiple tests scenarios

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -11,12 +11,35 @@ def test_execute_single_test_scenario(_, mock):
         'headers': {'Content-type': 'application/json'},
         'body': {'username': 'pyhttptest'}
     }
-    test_kwargs = {
+    json_data = {
         'name': 'TEST: List all users',
         'verb': 'GET',
         'endpoint': 'users',
         'host': 'http://localhost:8080',
     }
-    result = wrappers.execute_single_test_scenario(test_kwargs)
+    result = wrappers.execute_single_test_scenario(json_data)
 
     assert 'name' in result
+
+
+@patch('pyhttptest.wrappers.core.extract_http_response_content', return_value=dict)
+@patch('pyhttptest.wrappers.core.send_http_request')
+def test_execute_multiple_test_scenarios(_, mock):
+    mock.return_value = {'status_code': 200}
+    list_of_dicts = [
+        {
+            'name': 'TEST: List all users',
+            'verb': 'GET',
+            'endpoint': 'users',
+            'host': 'http://localhost:8080',
+        },
+        {
+            'name': 'TEST: Delete a user',
+            'verb': 'DELETE',
+            'endpoint': 'users/1',
+            'host': 'http://localhost:8080',
+        }
+    ]
+    results = wrappers.execute_multiple_test_scenarios(list_of_dicts)
+
+    assert len(results) == 2
