12dc1b5e67572b7823be01c6815bdfc9c2dea033
slaily <slavov.iliyan96@gmail.com>
2019-12-04 12:25:57 +0200

    Implement a wrapper function that executes single or multiple test scenarios according to the content from file

--- pyhttptest/wrappers.py
+++ pyhttptest/wrappers.py
@@ -1,4 +1,5 @@
 from pyhttptest import core
+from pyhttptest.printer import prepare_data_for_print
 
 
 def execute_single_test_scenario(json_data):
@@ -17,7 +18,7 @@ def execute_single_test_scenario(json_data):
     # Add a JSON property 'name' in the content
     response_content['name'] = json_data['name']
 
-    return response_content
+    return [response_content]
 
 
 def execute_multiple_test_scenarios(list_of_dicts):
@@ -34,3 +35,39 @@ def execute_multiple_test_scenarios(list_of_dicts):
     ]
 
     return results
+
+
+# ===========================
+# CLI
+# ===========================
+
+
+def _execute(file):
+    """Wrapper function that executes single or multiple
+    test scenarios according to the content from file.
+
+    :param str file_path: Optional file path.
+
+    :returns: Text output with the result.
+    :rtype: `str'
+    """
+    try:
+        list_of_content = core.load_content_from_json_file(file)
+        # Actually, the zeroth index contains the content
+        content = list_of_content[0]
+
+        # Depends on a type of the content loaded from the file,
+        # the business logic for single or multiple test scenarios
+        # is executed.
+        if isinstance(content, dict):
+            list_of_result = execute_single_test_scenario(content)
+        elif isinstance(content, list):
+            list_of_result = execute_multiple_test_scenarios(content)
+        else:
+            return 'The content from the file is not correctly formatted.'
+
+        output_for_print = prepare_data_for_print(list_of_result)
+    except Exception as exc:
+        return str(exc)
+
+    return output_for_print
